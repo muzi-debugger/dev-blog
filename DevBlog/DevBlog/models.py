211df50d4a05dec7b606348d95e29df021961298
muzi-debugger <mmzolodev@gmail.com>
2025-01-16 16:38:17 +0200

    Add model and migrations

---
+++ DevBlog/DevBlog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    slug = models.SlugField() # for url
+    date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
